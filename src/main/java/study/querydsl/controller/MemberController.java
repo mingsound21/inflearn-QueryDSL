5cc13919a6025079357d229420d465d41b825ea1
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-01-11 00:35:26 +0900

    Ch 06. 스프링 데이터 페이징 활용3 - 컨트롤러 개발
    파라미터로 Pageable
    
    uri에 page=?&size=? 작성!

--- src/main/java/study/querydsl/controller/MemberController.java
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -1,21 +1,42 @@
 package study.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.repository.MemberJpaRepository;
+import study.querydsl.repository.MemberRepository;
+import study.querydsl.repository.MemberRepositoryCustom;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
-    private final MemberJpaRepository memberJpaRepository;
+    private final MemberJpaRepository memberJpaRepository; // 순수 JPA 레포지토리 + querydsl
+    private final MemberRepository memberRepository; // 스프링 데이터 JPA + querydsl
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition){
         return memberJpaRepository.searchByWhere(condition);
+    }
+
+    /**
+     * 간단한 페이징
+     */
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable){
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    /**
+     * 복잡한 페이징
+     */
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable){
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
