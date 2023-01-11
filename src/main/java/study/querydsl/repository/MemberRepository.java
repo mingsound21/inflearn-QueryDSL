9af7ff9e4412e0679d96f894cbf468eeff7ae49a
mingsound <62479120+mingsound21@users.noreply.github.com>
2023-01-11 15:11:56 +0900

    Ch 07. 인터페이스 지원 - QuerydslPredicateExecutor
    JpaRepository를 상속받은 MemberRepository에 QuerydslPredicateExecutor도 상속 하면 된다.
    
    findAll, findOne등의 기본적인 crud 함수에 인자로 조건식인 predicate를 넘기면 알아서 querydsl을 사용해서 동적 쿼리 생성해줌.
    
    * 단! left join을 지원하지 않아 실무 사용은 어렵다.

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,11 +1,12 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom { // 인터페이스 다중 상속 가능
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> { // 인터페이스 다중 상속 가능
     // 정적 쿼리 - 메소드 이름으로 JPQL 쿼리 생성
     // select m from Member m where m.username = ?;
     List<Member> findByUsername(String username);
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -117,5 +118,36 @@ class MemberRepositoryTest {
         // then
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3"); // limit 3이니까
+    }
+
+    @Test
+    public void querydslPredicateExecutorTest() throws Exception {
+        // given
+        // 팀 2개 생성, 저장
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        // 멤버 4명 생성, 저장
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+        
+        //when
+        
+        QMember member = QMember.member;
+        // QuerydslPredicateExecutor를 extends 해서 findAll(조건식) 가능
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+        for (Member findMember : result) {
+            System.out.println("findMember = " + findMember);
+        }
     }
 }
